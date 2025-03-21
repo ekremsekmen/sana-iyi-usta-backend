d9f2710896d639b73b62bd877853265906873cd3
ekremsekmen <ekremsekmenq@gmail.com>
2025-03-21 03:24:25 +0300

    mimari oluşturuldu

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './modules/auth/auth.module';
+import { UsersModule } from './modules/users/users.module';
+import { MechanicsModule } from './modules/mechanics/mechanics.module';
+import { AppointmentsModule } from './modules/appointments/appointments.module';
+import { VehiclesModule } from './modules/vehicles/vehicles.module';
+import { CampaignsModule } from './modules/campaigns/campaigns.module';
+import { NotificationsModule } from './modules/notifications/notifications.module';
+import { MessagesModule } from './modules/messages/messages.module';
+import { ReviewsModule } from './modules/reviews/reviews.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule, MechanicsModule, AppointmentsModule, VehiclesModule, CampaignsModule, NotificationsModule, MessagesModule, ReviewsModule],
   controllers: [AppController],
   providers: [AppService],
 })
