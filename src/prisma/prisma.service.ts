db6f3859d44c93ec8fbf5bc25eeebc44bfe0038e
ekremsekmen <ekremsekmenq@gmail.com>
2025-03-28 01:57:41 +0300

    Integrate Prisma into the project

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,20 +1,28 @@
-// src/prisma/prisma.service.ts
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  OnModuleInit,
+  OnApplicationShutdown,
+} from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient {
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnApplicationShutdown
+{
   constructor() {
-    super();
+    super({
+      log: ['query', 'info', 'warn', 'error'],
+    });
   }
 
   async onModuleInit() {
     await this.$connect();
-    console.log('Prisma client connected');
+    console.log('✅ Prisma client connected');
   }
 
-  async onModuleDestroy() {
+  async onApplicationShutdown() {
     await this.$disconnect();
-    console.log('Prisma client disconnected');
+    console.log('❌ Prisma client disconnected');
   }
 }
